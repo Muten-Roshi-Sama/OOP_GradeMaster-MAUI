8253fa68fc71fab396f852523b56ede6fb4a69f9
Muten-Roshi-Sama <21245@ecam.be>
2023-12-27 13:57:26 +0100

    added Services folder and temporary DataInitializer.

---
+++ GradeMasterMAUI/GradeMasterMAUI/Services/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeMasterMAUI.Services
+{
+    //From ChatGPT
+    public static class Config
+    {
+        public static string RootDir => Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            ".GradeMasterMAUI");
+    }
+}
