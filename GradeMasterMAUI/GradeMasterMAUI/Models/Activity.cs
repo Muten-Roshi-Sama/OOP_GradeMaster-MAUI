e73df5286d0fb757b5306ab98b66d716f859331c
Muten-Roshi-Sama <21245@ecam.be>
2023-12-28 21:45:00 +0100

    Updated one file per class following th SOLID SRP.

---
+++ GradeMasterMAUI/GradeMasterMAUI/Models/Activity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeMasterMAUI.Models
+{
+    public class Activity
+    {
+        //possede un unique enseignant
+        public int ECTS;
+        public string activityName;
+        public Professor professor; //activity has-a professor (composition).
+
+        public Activity(string activityName, Professor professor, int ECTS)
+        {
+            this.activityName = activityName;
+            this.professor = professor;
+            this.ECTS = ECTS;
+        }
+    }
+
+
+}
---
+++ GradeMasterMAUI/GradeMasterMAUI/Services/OnDataChanged.cs
@@ -0,0 +1,13 @@
+﻿namespace GradeMasterMAUI.Services
+{
+    public class DataChangedNotifier
+    {
+        public static event Action OnDataChanged;
+
+        public static void NotifyDataChanged()
+        {
+            OnDataChanged?.Invoke();
+        }
+    }
+
+}
