b280704c035013469b62b653c0439512a676d953
Muten-Roshi-Sama <21245@ecam.be>
2024-01-22 17:56:46 +0100

    AES encryption for FileEncryptionService.

--- GradeMasterMAUI/GradeMasterMAUI/Services/FileAccessService.cs
+++ GradeMasterMAUI/GradeMasterMAUI/Services/FileAccessService.cs
@@ -1,60 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GradeMasterMAUI.Services;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Maui.Storage;
 
-namespace GradeMasterMAUI.Services
+public static class FileEncryptionService
 {
-    public static class FileAccessService
+    private static readonly string KeyName = "AesEncryptionKey";
+
+    public static void InitializeEncryptionKey()
     {
-        //private static readonly object FileLock = new();
-        private static readonly SemaphoreSlim FileSemaphore = new SemaphoreSlim(1, 1);
-
-        public static async Task<string> ReadFileAsync(string path, string errorOrigin)
+        if (SecureStorage.GetAsync(KeyName).Result == null)
         {
-            await FileSemaphore.WaitAsync();
-            try
+            using (var aes = Aes.Create())
             {
-                Config.EnsureDirectory();
-                if (!File.Exists(path))
-                {
-                    throw new FileNotFoundException($"The file at {path} was not found.[Origin : FileAccessService-{errorOrigin}]");
-                }
-
-                // Decrypt, Read, Encrypt
-                await FileEncryptionService.DecryptFileAsync(inputFile: path, outputFile: path);
-                string fileContent = File.ReadAllText(path);
-                await FileEncryptionService.EncryptFileAsync(inputFile: path, outputFile: path);
-
-                return fileContent;
-            }
-            finally
-            {
-                FileSemaphore.Release();
+                aes.GenerateKey();
+                string base64Key = Convert.ToBase64String(aes.Key);
+                SecureStorage.SetAsync(KeyName, base64Key).Wait();
             }
         }
+    }   //TODO: add this to Config.EnsureDir()
 
-        public static async Task WriteFileAsync(string path, string content, string origin)
-        {
-            await FileSemaphore.WaitAsync();
-            try
-            {
-                // Decrypt the file before writing.
-                await FileEncryptionService.DecryptFileAsync(inputFile: path, outputFile: path);
-                File.WriteAllText(path, content);
-                // Encrypt the file after writing.
-                await FileEncryptionService.EncryptFileAsync(inputFile: path, outputFile: path);
-            }
-            finally
-            {
-                FileSemaphore.Release();
-            }
-        }
-
-        // Add other file operations as needed (e.g., DeleteFile, AppendToFile, etc.)
+    private static byte[] GetAesKey()
+    {
+        string base64Key = SecureStorage.GetAsync(KeyName).Result;
+        return Convert.FromBase64String(base64Key);
     }
 
+    public static string EncryptText(string text)
+    {
+        byte[] key = GetAesKey();
+        using (var aes = Aes.Create())
+        {
+            aes.Key = key;
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                using (var sw = new StreamWriter(cs))
+                {
+                    sw.Write(text);
+                }
+
+                byte[] iv = aes.IV;
+                byte[] encrypted = ms.ToArray();
+                byte[] result = new byte[iv.Length + encrypted.Length];
+                Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                Buffer.BlockCopy(encrypted, 0, result, iv.Length, encrypted.Length);
+
+                return Convert.ToBase64String(result);
+            }
+        }
+    }
+
+    public static string DecryptText(string cipherText)
+    {
+        byte[] fullCipher = Convert.FromBase64String(cipherText);
+        byte[] iv = new byte[16];
+        byte[] cipher = new byte[fullCipher.Length - iv.Length];
+
+        Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
+        Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, cipher.Length);
+
+        using (var aes = Aes.Create())
+        {
+            aes.Key = GetAesKey();
+            aes.IV = iv;
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using (var ms = new MemoryStream(cipher))
+            using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+            using (var sr = new StreamReader(cs))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
 }
